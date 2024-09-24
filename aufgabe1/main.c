694d1cf5df23d0132603793e6348888c0edce698
= <=>
2024-09-24 19:53:12 +0200

    fixed some double rounding errors

--- aufgabe1/main.c
+++ aufgabe1/main.c
@@ -1,36 +1,38 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main()
-{
-    int width;
-    int length;
-    int prospects;
+int main() {
+  int width;
+  int length;
+  int prospects;
 
-    printf("Bitte gebe die Breite des Grundstücks an: ");
-    scanf("%d", &width);
+  printf("Bitte gebe die Breite des Grundstücks an: ");
+  scanf("%d", &width);
 
-    printf("\nBitte gebe die Länge des Grundstücks an: ");
-    scanf("%d", &length);
+  printf("\nBitte gebe die Länge des Grundstücks an: ");
+  scanf("%d", &length);
 
-    printf("\nBitte gebe die Anzahl der Interessenten des Grundstücks an: ");
-    scanf("%d", &prospects);
+  printf("\nBitte gebe die Anzahl der Interessenten des Grundstücks an: ");
+  scanf("%d", &prospects);
 
-    double lot_width;
-    double lot_lenght;
 
-    double n = 0;
+  double lot_width = ceil(sqrt(prospects * width / length));
 
-    do
-    {
-        lot_width = ceil(sqrt(prospects * (width/length + n)));
+  double lot_lenght = ceil(prospects / lot_width);
 
-        lot_lenght = ceil(prospects/lot_width);
+  printf("\nBreite: %f, Länge: %f", lot_width, lot_lenght);
 
-        printf("\nBreite: %f, Länge: %f", lot_width, lot_lenght);
+  double n = 0.0;
 
-        printf("\nn: %f", n);
-        n += 0.05;
-    }
-    while (lot_width * lot_lenght > prospects * 1.1);
+  while (lot_width * lot_lenght > prospects * 1.1)
+  {
+    lot_width = ceil(sqrt(prospects * (width / length + n)));
+
+    lot_lenght = ceil(prospects / lot_width);
+
+    printf("\nBreite: %f, Länge: %f", lot_width, lot_lenght);
+
+    printf("\nn: %f", n);
+    n += 0.05;
+  } 
 }
