1ef23d227298f9d60d527afb7de771e9577c0fcd
= <=>
2024-09-28 13:39:08 +0200

    renamed stuff

--- aufgabe1/main.c
+++ aufgabe1/main.c
@@ -1,38 +1,6 @@
-#include <math.h>
 #include <stdio.h>
 
-int main() {
-  int width;
-  int length;
-  int prospects;
-
-  printf("Bitte gebe die Breite des Grundstücks an: ");
-  scanf("%d", &width);
-
-  printf("\nBitte gebe die Länge des Grundstücks an: ");
-  scanf("%d", &length);
-
-  printf("\nBitte gebe die Anzahl der Interessenten des Grundstücks an: ");
-  scanf("%d", &prospects);
-
-
-  double lot_width = ceil(sqrt(prospects * width / length));
-
-  double lot_lenght = ceil(prospects / lot_width);
-
-  printf("\nBreite: %f, Länge: %f", lot_width, lot_lenght);
-
-  double n = 0.0;
-
-  while (lot_width * lot_lenght > prospects * 1.1)
-  {
-    lot_width = ceil(sqrt(prospects * (width / length + n)));
-
-    lot_lenght = ceil(prospects / lot_width);
-
-    printf("\nBreite: %f, Länge: %f", lot_width, lot_lenght);
-
-    printf("\nn: %f", n);
-    n += 0.05;
-  } 
+int main()
+{
+    printf("Hello world!");
 }
